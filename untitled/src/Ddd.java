85d92fb9950a403a2587773eb06c7cb40c3345b8
Ribeiro Pedro <phcribeiro123@gmail.com>
2026-09-04 22:34:23 -0300

    Exercicios Teste

---
+++ untitled/src/Ddd.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ddd {
+
+    public static void main(String[] args) throws IOException {
+            Scanner sc= new Scanner(System.in);
+            int n= sc.nextInt();
+
+            if (n==11){
+                System.out.println("Sao Paulo");
+            } else if (n==61) {
+                System.out.println("Brasilia");
+            } else if (n==71) {
+                System.out.println("Salvador");
+            } else if (n==21) {
+                System.out.println("Rio de Janeiro");
+            } else if (n==32) {
+                System.out.println("Juiz de Fora");
+            } else if (n==19) {
+                System.out.println("Campinas");
+            } else if (n==27) {
+                System.out.println("Vitoria");
+            } else if (n==31) {
+                System.out.println("Belo Horizonte");
+            } else{
+                System.out.println("DDD nao cadastrado");
+            }
+
+    }
+
+}
